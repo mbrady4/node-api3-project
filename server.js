38d8b37f8f3d828ddbcf22fb7cc548eaf8d016e9
Michael Brady <mikebrady44@gmail.com>
2020-06-12 14:40:24 -0400

    Prepared API for deployment to Heroku

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const userRoutes = require("./users/userRouter");
 const postRoutes = require("./posts/postRouter");
 
 const server = express();
+
+const port = process.env.PORT;
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -17,9 +21,9 @@ function logger(req, res, next) {
 }
 
 server.use(logger);
-server.use('/api/users', userRoutes);
-server.use('/api/posts', postRoutes);
+server.use("/api/users", userRoutes);
+server.use("/api/posts", postRoutes);
 
-server.listen(8000, () => console.log("Blog API running on port 8000."));
+server.listen(port, () => console.log(`Blog API running on port ${port}.`));
 
 module.exports = server;
